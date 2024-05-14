07dc039d3518520096169f8489a86438983e71e1
trainthunder <kitikun.po@gmail.com>
2024-05-14 12:30:17 +0700

    feat:train Hackhour

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,29 @@
 // Start coding here
+
+//Add Start
+const add = (a, b) => {
+  return a + b;
+};
+console.log(add(10, 5));
+//Add End
+
+//Subtract Start
+const subtract = (a, b) => {
+  return a - b;
+};
+console.log(subtract(10, 5));
+//Subtract End
+
+//Multiply Start
+const multiply = (a, b) => {
+  return a * b;
+};
+console.log(multiply(10, 5));
+//Mutiply End
+
+//Divide Start
+const divide = (a, b) => {
+  return a / b;
+};
+console.log(divide(10, 5));
+//Divide End
